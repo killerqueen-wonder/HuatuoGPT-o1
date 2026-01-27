4bbf4001edd426434374de96d2e4ec107c000862
NEVVY <nevvy@qq.com>
2026-01-27 15:51:22 +0800

    1

--- training_data_mod.py
+++ training_data_mod.py
@@ -85,7 +85,7 @@ def extract_numbers(text):
             pass
 
     print(f'[debug] 匹配数字列表失败，模型回复为：{text}')
-    return []
+    return None
 
 def filter_LLM(information: list, thinking: str, llm: LLM):
     """
@@ -103,14 +103,20 @@ def filter_LLM(information: list, thinking: str, llm: LLM):
 {information}
 
 请找出在思维链中没有被提及、且对得出结论没有帮助的文档编号。
-输出要求：仅输出无关文档的阿拉伯数字编号列表，例如 [1, 3]。如果全部相关，输出 []。注意：直接输出列表，不要给出任何解释。"""
+输出要求：仅输出无关文档的阿拉伯数字编号列表，例如 [1, 3]。如果全部相关，输出 []。注意：直接输出列表，不要给出任何解释。  /no_think"""
 
     res = llm.gen(prompt)
-    print(f"[Debug] 模型原始回复: {res}")
-
     fail_indices = extract_numbers(res)
     # 根据模型返回的“无关编号”进行过滤
     # 注意：模型返回的是 1-based index
+
+    # if fail_indices == None:
+    #     #可能被截断，用无思考模式
+    #     prompt=prompt + "  /no_think"
+    #     res = llm.gen(prompt)
+    #     fail_indices = extract_numbers(res)
+
+    print(f"[Debug] 模型原始回复: {res}")    
     filtered_info = [item for i, item in enumerate(information, start=1) if i not in fail_indices]
     
     return filtered_info
